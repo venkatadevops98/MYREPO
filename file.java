d58304d7dddd3b6d5abaa9edf18ea38ade7451bf
root <root@ip-172-31-27-253.ec2.internal>
2025-11-05 18:00:42 +0000

    Merge conflit trobleshooted successfully

--- file.java
+++ file.java
@@ -5,5 +5,11 @@ This is a multi-line comment.
 It prints "Hello, World!" to the browser's console.
 */
 
+<<<<<<< Updated upstream
 Testing merge coflict
  this the latest commit in central Repo
+=======
+This is to test merge conflit
+
+without pull from central repo give commit
+>>>>>>> Stashed changes
